b46a8c958385578849b9292dbbbe65b6e6b072a4
Tsai Wenja <agon978@gmail.com>
2023-06-10 17:22:15 +0800

    feature: toggle dropdowns in navbar by mouse hovering

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,10 @@ AOS.init();
 $('#sec-top > video').on('loadeddata', function(e) {
   $('#navbar1.enter-animation').addClass('show');
 });
+
+$('.nav-item.dropdown')
+  .on('mouseenter', function(e) {
+    $(this).find('[data-bs-toggle="dropdown"]').dropdown('show');
+  }).on('mouseleave', function(e) {
+    $(this).find('[data-bs-toggle="dropdown"]').dropdown('hide');
+  });
