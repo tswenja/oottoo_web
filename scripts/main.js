6581997d96835765f52fad518a7730a2e214f266
Tsai Wenja <agon978@gmail.com>
2023-06-14 01:20:48 +0800

    Fix autoplay

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,16 @@ AOS.init();
 $('#sec-top > video').on('loadeddata', function(e) {
   $('.enter-animation').addClass('show');
   console.log('loaded');
+  stepingVideo(e.target, 0.1);
 });
 $('#sec-top > video')[0].load();
+
+function stepingVideo(video, step) {
+  video.currentTime += step;
+
+  if (video.currentTime >= video.duration) return;
+  setTimeout(stepingVideo.bind(null, video, step), step * 1000);
+}
 
 $('.nav-item.dropdown')
   .on('mouseenter', function(e) {
